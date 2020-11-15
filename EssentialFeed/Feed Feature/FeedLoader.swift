222ef954a8d3299ec8f555b83c712e38338f75a3
Min <qwer810520@gmail.com>
2020-11-15 17:03:18 +0800

    Replace  enum with the pre defined  enum from the Feed module(in preparartion to conform to the  protocol)

--- EssentialFeed/Feed Feature/FeedLoader.swift
+++ EssentialFeed/Feed Feature/FeedLoader.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+//public typealias LoadFeedResult = Swift.Result<[FeedItem], Error>
+
+public enum LoadFeedResult<Error: Swift.Error> {
+  case success([FeedItem])
+  case failure(Error)
+}
+
+extension LoadFeedResult: Equatable where Error: Equatable {
+
+}
+
 protocol FeedLoader {
-  func load(completion: @escaping (Result<[FeedItem], Error>) -> Void)
+  associatedtype Error: Swift.Error
+
+  func load(completion: @escaping (LoadFeedResult<Error>) -> Void)
 }
