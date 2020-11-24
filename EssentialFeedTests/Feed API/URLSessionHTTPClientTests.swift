01a2bdedd9e90a9971a9ead06addd26b81cbacfe
Min <qwer810520@gmail.com>
2020-11-24 16:11:34 +0800

    Add nil data and nil response to the request error test case to make the setup more explicit

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -32,7 +32,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     URLProtocolStub.startInterceptingRequests()
     let url = URL(string: "http://any-url.com")!
     let error = NSError(domain: "any error", code: 1)
-    URLProtocolStub.stub(url: url, error: error)
+    URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
 
     let sut = URLSessionHTTPClient()
 
@@ -61,11 +61,13 @@ class URLSessionHTTPClientTests: XCTestCase {
     private static var stubs = [URL: Stub]()
 
     private struct Stub {
+      let data: Data?
+      let response: URLResponse?
       let error: Error?
     }
 
-    static func stub(url: URL, error: Error? = nil) {
-      stubs[url] = Stub(error: error)
+    static func stub(url: URL, data: Data?, response: URLResponse?, error: Error? = nil) {
+      stubs[url] = Stub(data: data, response: response, error: error)
     }
 
     static func startInterceptingRequests() {
@@ -89,6 +91,15 @@ class URLSessionHTTPClientTests: XCTestCase {
 
     override func startLoading() {
       guard let url = request.url, let stubs = URLProtocolStub.stubs[url] else { return }
+
+      if let data = stubs.data {
+        client?.urlProtocol(self, didLoad: data)
+      }
+
+      if let response = stubs.response {
+        client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .allowed)
+      }
+
       if let error = stubs.error {
         client?.urlProtocol(self, didFailWithError: error)
       }
