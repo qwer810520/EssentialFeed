d3de99c3ddf81b7996a2abacf6a644b8885c381b
Min <qwer810520@gmail.com>
2020-11-26 20:37:30 +0800

    Delivers unexpected values error when request completes with data, response and error as nil

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -16,10 +16,14 @@ class URLSessionHTTPClient {
     self.session = session
   }
 
+  struct UnexpectedValuesRepresentation: Error { }
+
   func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
     session.dataTask(with: url) { _, _, error in
       if let error = error {
         completion(.failure(error))
+      } else {
+        completion(.failure(UnexpectedValuesRepresentation()))
       }
 
     }.resume()
@@ -53,7 +57,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     wait(for: [exp], timeout: 1.0)
   }
 
-  func test_fetFromURL_failsOnRequestError() {
+  func test_getFromURL_failsOnRequestError() {
     let error = NSError(domain: "any error", code: 1)
     URLProtocolStub.stub(data: nil, response: nil, error: error)
 
@@ -67,6 +71,24 @@ class URLSessionHTTPClientTests: XCTestCase {
           XCTAssertEqual(receivedError.domain, error.domain)
         default:
           XCTFail("Expected failed with error \(error), got \(result) instead")
+      }
+
+      exp.fulfill()
+    }
+
+    wait(for: [exp], timeout: 1.0)
+  }
+
+  func test_getFromURL_failsOnAllNilValues() {
+    URLProtocolStub.stub(data: nil, response: nil, error: nil)
+
+    let exp = expectation(description: "Wait for completion")
+
+    makeSUT().get(from: anyURL()) { result in
+      switch result {
+        case .failure: break
+        default:
+          XCTFail("Expected failed, got \(result) instead")
       }
 
       exp.fulfill()
