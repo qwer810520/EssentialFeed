0d780c16502e727843693317a037f6b086193d65
Min <qwer810520@gmail.com>
2021-03-07 21:13:25 +0800

    Load Command fails on retrieval error

--- EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -18,8 +18,10 @@ class FeedStoreSpy: FeedStore {
   }
 
   private(set) var receivedMessages = [ReceivedMessage]()
+
   private var deletionCompletions = [DeletionCompletion]()
   private var insertionCompletions = [InsertionCompletion]()
+  private var retrievalCompletions = [RetrievalCompletion]()
 
   func deletecachedFeed(completion: @escaping DeletionCompletion) {
     deletionCompletions.append(completion)
@@ -48,7 +50,12 @@ class FeedStoreSpy: FeedStore {
     insertionCompletions[index](nil)
   }
 
-  func retrieve() {
+  func retrieve(completion: @escaping RetrievalCompletion) {
+    retrievalCompletions.append(completion)
     receivedMessages.append(.retrieve)
+  }
+
+  func completeRetrieval(with error: Error, at index: Int = 0) {
+    retrievalCompletions[index](error)
   }
 }
