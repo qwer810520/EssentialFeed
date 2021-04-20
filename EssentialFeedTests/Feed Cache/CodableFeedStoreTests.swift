331a94add717545e1e5ce1442c9c24a1e07ec997
Min <qwer810520@gmail.com>
2021-04-20 14:23:53 +0800

    Extract duplicate insert test code  into a resusable helper method

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -99,13 +99,8 @@ class CodableFeedStoreTests: XCTestCase {
     let sut = makeSUT()
     let feed = uniqueImageFeed().local
     let timestamp = Date()
-
-    let exp = expectation(description: "Wait for cache retrieval")
-    sut.insert(feed, timestamp: timestamp) { insertionError in
-      XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
-      exp.fulfill()
-    }
-    wait(for: [exp], timeout: 1.0)
+    
+    insert((feed, timestamp), to: sut)
 
     expect(sut, toRetrieve: .found(feed: feed, timestamp: timestamp))
   }
@@ -115,12 +110,7 @@ class CodableFeedStoreTests: XCTestCase {
     let feed = uniqueImageFeed().local
     let timestamp = Date()
 
-    let exp = expectation(description: "Wait for cache retrieval")
-    sut.insert(feed, timestamp: timestamp) { insertionError in
-      XCTAssertNil(insertionError, "Expected feed to be inserted successfully")
-      exp.fulfill()
-    }
-    wait(for: [exp], timeout: 1.0)
+    insert((feed, timestamp), to: sut)
 
     expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
   }
@@ -131,6 +121,15 @@ class CodableFeedStoreTests: XCTestCase {
     let sut = CodableFeedStore(storeURL: testSpecificStoreURL())
     trackForMemoryLeaks(sut, file: file, line: line)
     return sut
+  }
+
+  private func insert(_ cache: (feed: [LocalFeedImage], timestamp: Date), to sut: CodableFeedStore) {
+    let exp = expectation(description: "Wait for cache insertion")
+    sut.insert(cache.feed, timestamp: cache.timestamp) { insertionError in
+      XCTAssertNil(insertionError, "Expected feeed to be inserted successfully")
+      exp.fulfill()
+    }
+    wait(for: [exp], timeout: 1.0)
   }
 
   private func expect(_ sut: CodableFeedStore, toRetrieveTwice expectedResult: RetrieveCachedFeedResult, file: StaticString = #filePath, line: UInt = #line) {
