120c6e10326a14656e7386bba5139a41da635dd1
Min <qwer810520@gmail.com>
2021-04-26 16:09:30 +0800

    'CoreDataFeedStore.retrieve()' delivers found values on non-empty cache

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -19,11 +19,45 @@ public final class CoreDataFeedStore: FeedStore {
   }
 
   public func retrieve(completion: @escaping RetrievalCompletion) {
-    completion(.empty)
+    let context = self.context
+    context.perform {
+      do {
+        let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name ?? "")
+        request.returnsObjectsAsFaults = false
+        if let cache = try context.fetch(request).first {
+          completion(.found(feed: cache.feed
+                              .compactMap { ($0 as? ManagedFeedImage) }
+                              .map { LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url) },
+                            timestamp: cache.timestamp))
+        } else {
+          completion(.empty)
+        }
+      } catch {
+        completion(.failure(error))
+      }
+    }
   }
 
   public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-
+    let context = self.context
+    context.perform {
+      do {
+        let managedCache = ManagedCache(context: context)
+        managedCache.timestamp = timestamp
+        managedCache.feed = NSOrderedSet(array: feed.map { local in
+          let managed = ManagedFeedImage(context: context)
+          managed.id = local.id
+          managed.imageDescription = local.description
+          managed.location = local.location
+          managed.url = local.url
+          return managed
+        })
+        try context.save()
+        completion(nil)
+      } catch {
+        completion(error)
+      }
+    }
   }
 
   public func deletecachedFeed(completion: @escaping DeletionCompletion) {
@@ -63,19 +97,19 @@ private extension NSManagedObjectModel {
   }
 }
 
-//@objc(ManagedFeedImage)
-//internal class ManagedCache: NSManagedObject {
-//  @NSManaged internal var timestamp: Date
-//  @NSManaged internal var feed: NSOrderedSet
-//}
-//
-//@objc(ManagedCache)
-//internal class ManagedFeedImage: NSManagedObject {
-//  @NSManaged internal var id: UUID
-//  @NSManaged internal var imageDescription: String?
-//  @NSManaged internal var location: String?
-//  @NSManaged internal var url: URL
-//  @NSManaged internal var cache: ManagedCache
-//}
+@objc(ManagedFeedImage)
+private class ManagedCache: NSManagedObject {
+  @NSManaged var timestamp: Date
+  @NSManaged var feed: NSOrderedSet
+}
+
+@objc(ManagedCache)
+private class ManagedFeedImage: NSManagedObject {
+  @NSManaged var id: UUID
+  @NSManaged var imageDescription: String?
+  @NSManaged var location: String?
+  @NSManaged var url: URL
+  @NSManaged var cache: ManagedCache
+}
 
 
