9a831cd77f17742a7f89d16d2592d4e32e75b220
Min <qwer810520@gmail.com>
2021-06-07 15:06:16 +0800

    Remove mutable state from `FeedViewModel`. The `FeedViewModel` only needs to forward state changes, so state is only translent.

--- EssentialFeediOS/FeedUI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeediOS/FeedUI/Controllers/FeedRefreshViewController.swift
@@ -28,11 +28,11 @@ final class FeedRefreshViewController: NSObject {
   // MARK: - Private Methods
 
   private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
-    viewModel.onChange = { [weak self] viewModel in
-      if viewModel.isLoading {
-        self?.view.beginRefreshing()
+    viewModel.onLoadingStateChange = { [weak view] isLoading in
+      if isLoading {
+        view?.beginRefreshing()
       } else {
-        self?.view.endRefreshing()
+        view?.endRefreshing()
       }
     }
 
--- EssentialFeediOS/FeedUI/Models/FeedViewModel.swift
+++ EssentialFeediOS/FeedUI/Models/FeedViewModel.swift
@@ -11,13 +11,10 @@ import EssentialFeed
 
 final class FeedViewModel {
 
+  typealias Observer<T> = (T) -> Void
 
-  var onChange: ((FeedViewModel) -> Void)?
-  var onFeedLoad: (([FeedImage]) -> Void)?
-
-  private(set) var isLoading: Bool = false {
-    didSet { onChange?(self) }
-  }
+  var onLoadingStateChange: Observer<Bool>?
+  var onFeedLoad: Observer<[FeedImage]>?
 
   private let feedLoader: FeedLoader
 
@@ -26,13 +23,13 @@ final class FeedViewModel {
   }
 
   func loadFeed() {
-    self.isLoading = true
+    onLoadingStateChange?(true)
     feedLoader.load { [weak self] result in
       if let feed = try? result.get() {
         self?.onFeedLoad?(feed)
       }
 
-      self?.isLoading = false
+      self?.onLoadingStateChange?(false)
     }
   }
 }
